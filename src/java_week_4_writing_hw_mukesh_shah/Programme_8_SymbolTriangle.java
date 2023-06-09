d45034083f280cb0bc25e87920449d779a41d4d2
indiamogambo3 <indiamogambo3@gmail.com>
2023-06-09 15:33:40 +0100

    Homework complete

---
+++ src/java_week_4_writing_hw_mukesh_shah/Programme_8_SymbolTriangle.java
@@ -0,0 +1,40 @@
+package java_week_4_writing_hw_mukesh_shah;
+
+import java.util.Scanner;
+
+/**
+ * Display right angle triangle of @ using nested for loops
+ *
+ * @
+ * @@
+ * @@@
+ * @@@@
+ * @@@@@
+ */
+
+public class Programme_8_SymbolTriangle {
+
+    public static void symbolTriangle(int n) {
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("@");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input number of rows: ");
+
+        symbolTriangle(scanner.nextInt());
+
+        //closing the scanner object
+        scanner.close();
+    }
+
+}
---
+++ src/programme_24_method_overriding/AXIS.java
@@ -0,0 +1,10 @@
+package programme_24_method_overriding;
+
+public class AXIS extends Bank {
+
+    public int getRateOfInterest() {
+
+        return 9;
+    }
+
+}
